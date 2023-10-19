64d762d0636ad5c40788af66496004bc71a98056
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-10-19 18:54:51 -0500

    Lab 8 Commit - Test #1

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,4 +1,15 @@
-
 function getDataFromForm() {
   alert("it worked!");
+  var fname = document.getElementsByName("fname");
+  var lname = document.getElementsByName("lname");
+  runAjax(fname, lname);
+}
+
+function runAjax(fname, lname) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("stringResponse").innerHTML = this.responseText;
+    }
+  xhttp.open("GET", "ajax.php" + "?fname=" + fname + "&lname=" + lname, true);
+  xhttp.send();
 }
