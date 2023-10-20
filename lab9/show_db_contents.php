d1175aa3d9e519d8eaee1cebb813ac6386d6c76d
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-10-20 04:05:32 -0500

    Lab Commit 9 - Test 11

--- lab9/show_db_contents.php
+++ lab9/show_db_contents.php
@@ -9,20 +9,12 @@ $db_user="dglqqslili";        //Change this
 $db_pass="cs-4413-002";        //Change this
 $db_name="z_url_set_1";     //Do not change
 
-$connectionOptions = array(
-    "Database" => "z_url_set_1", // update me
-    "Uid" => "dglqqslili", // update me
-    "PWD" => "cs-4413-002" // update me
-);
-//Establishes the connection
-$db_conn = sqlsrv_connect($db_host, $connectionOptions);
-
-//$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-// if (mysqli_connect_errno())
-// {
-//     echo 'Connection to database failed:'.mysqli_connect_error();
-//     exit();
-// }
+$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name, "3306");
+if (mysqli_connect_errno())
+{
+    echo 'Connection to database failed:'.mysqli_connect_error();
+    exit();
+}
 
 echo "database connection success<br>";
 echo "<strong>now showing results from a database query...</strong>";
