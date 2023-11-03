02998b7bb95c1cf9948fc07ebac0e463579d6b7e
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-03 01:18:13 -0500

    Lab 10 Commit - Test 13

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,3 +1,14 @@
-<?php
-    echo "<p>TEST</p>"
-?>
+<html>
+    <body>
+        <?php
+            echo "<p>TEST</p>"
+            $myfile = fopen("data.txt", "rb") or die("Unable to open file!");
+            while (!feof($myfile) ) {
+                $line = fgets($myfile);
+                $parts = explode(' ', $line);
+                print $parts[0].$parts[1]."<br>";
+            }
+            fclose($myfile);
+        ?>
+    </body>
+</html>
