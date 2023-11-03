e1bb2284970ecb2d5176581fb01cf44cfd3af2bf
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-03 00:32:24 -0500

    Lab 10 Commit - Test 8

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -1,3 +1,7 @@
-<?php if (isset($_POST['submit'])) : ?>
-   <p>Thank you for subscribing!</p>
-<?php endif; ?>
+<?php
+    $myfile = fopen("data.txt", "w") or die("Unable to open file!");
+    $txt = $_POST['fname']." ".$_POST['lname']."\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    echo file_get_contents("/home/site/wwwroot/lab10");
+?> 
