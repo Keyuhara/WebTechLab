92b16641c094a511ddef9efccdd64efc32b936da
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-02 23:29:36 -0500

    Lab 10 Commit - Test 4

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -1,3 +1,8 @@
 <?php
-echo getcwd();
+$myfile = fopen("data.txt", "a+") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?> 
