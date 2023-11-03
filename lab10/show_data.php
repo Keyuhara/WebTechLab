e4059d8c6cbdd6eab3c36f371368700f891404bd
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-03 01:52:17 -0500

    Lab 10 Commit - Test 19

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,12 +1,16 @@
 <html>
     <body>
         <table>
+            <tr>
+                <td>First Name</td>
+                <td>Last Name</td>
+            <tr>
             <?php
                 $myfile = fopen("data.txt", "rb") or die("Unable to open file!");
                 while (!feof($myfile) ) {
                     $line = fgets($myfile);
                     $parts = explode(' ', $line);
-                    echo "<td>".$parts[0]."</td><td>".$parts[1]."</td><br>";
+                    echo "<tr><td>".$parts[0]."</td><td>".$parts[1]."</td></tr>";
                 }
                 fclose($myfile);
             ?>
