4531ead2728363c2a00c8396dcdfdd0cae7822e0
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-02 22:56:43 -0500

    Lab 10 Commit - Test 1

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -1,12 +1,5 @@
-<?php
-
-$myfile = fopen("data.txt", "a") or fopen("data.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-$txt = $_POST['fname']." ".$_POST['lname']."\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-
-?>
+<html>
+    <body>
+        Welcome <?php echo $_GET["fname"]; ?> <?php echo $_GET["lname"]; ?>
+    </body>
+</html> 
