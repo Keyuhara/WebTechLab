f7537cc9f5350f1c06f2384ab690372c914cf099
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-03 02:05:25 -0500

    Delete save_data.php

--- save_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $myfile = fopen("/home/site/wwwroot/lab10/data.txt", "a") or die("Unable to open file!");
-    $txt = $_POST['fname']." ".$_POST['lname']."\n";
-    fwrite($myfile, $txt);
-    fclose($myfile);
-    header("Location: https://cs-4413-002-fall-2023.azurewebsites.net/lab10/");
-?> 
