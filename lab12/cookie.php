17c26426e8965f3733f4c9529769629e0c01efbd
Keyuhara <81481387+Keyuhara@users.noreply.github.com>
2023-11-08 16:10:22 -0600

    Lab 12 Commit - cookie message clarification

--- lab12/cookie.php
+++ lab12/cookie.php
@@ -1,11 +1,11 @@
 <?php
-    setcookie("cs4413", "cs4413", time() + 100);
     if (isset($_COOKIE["cs4413"])) 
     {
         echo "Cookie is set!";
     }
     else
     { 
-        echo "Cookie is NOT set!";
+        echo "Cookie is NOT set! Refresh the page within 100 seconds.";
     } 
+    setcookie("cs4413", "cs4413", time() + 100);
 ?>
